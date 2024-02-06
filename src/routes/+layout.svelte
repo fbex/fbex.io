73ee85edb53d7e9a7bdbf43e442af8127dc1e885
fbex <8228317+fbex@users.noreply.github.com>
2024-02-06 20:45:02 +0100

    Add vercel adapter and vercel web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import '../app.css';
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	// use Vercel web analytics
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
